0d67245bda3ecd58a4d070b66e343645c8d0d884
valentina <valentinagiannone@gmail.com>
2020-04-07 23:14:47 +0200

    finish general page , now code cleaning

---
+++ OrnellaGiannone/resources/views/components/friendZone.blade.php
@@ -0,0 +1,27 @@
+<div class="box_friends" >
+    
+<div class="polaroid"   >
+    <div class="esterno"> 
+        <div class="interno">
+           <img src="/images/stefano.jpg" alt="">
+        </div>
+        <p><a href="https://www.stefanosalvadeo.it/">www.stefanosalvadeo.it</a></p>
+    </div>
+</div>
+<div class="polaroid">
+    <div class="esterno"> 
+        <div class="interno">
+             <img src="/images/reverie.png" alt="">
+        </div>
+        <p><a href="https://www.studioreveriemilano.com/"> www.studioreveriemilano.com</a></p>
+    </div>
+</div>
+<div class="polaroid">
+    <div class="esterno"> 
+        <div class="interno">
+            <img src="/images/studio.png" alt="">
+        </div>
+        <p><a href="https://www.diagnositerapiarozzano.it/"> www.diagnositerapiarozzano.it</a></p>
+    </div>
+</div>
+</div>
--- OrnellaGiannone/resources/views/components/main.blade.php
+++ OrnellaGiannone/resources/views/components/main.blade.php
@@ -1,7 +1,8 @@
     
    <div class="box_main_up">    
         <div class="box_main_left">  
-            <img src="/images/ornella.webp" alt=""></div>
+            <img src="/images/ornella.webp" alt="">
+        </div>
         <div class="box_main_right"> 
             <h1 class="animated fadeInRight">STUDIO DI PSICOLOGIA E PSICOTERAPIA</h1>  
             <h1 class="animated fadeInLeft">Dott.ssa Ornella Giannone</h1>       
@@ -13,11 +14,9 @@
         <h1>Prenota la tua prima visita</h1>
         <div class="btn_group">
             <div class="my_button">
-                
                 <a href="Tel:+393496376947"><i class="fas fa-phone"></i> Chiama</a>
             </div>
-            <div class="my_button">
-                
+            <div class="my_button">  
                 <a href="mailto:giannone.ornella@libero.it"><i class="fas fa-at"></i> Scrivimi</a>
              </div>
         </div>
